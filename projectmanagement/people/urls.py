a78190970364efdb155c98c6882adb93d5ca91ab
harshapalnatiunh <HarshavardhanReddy.Palnati@unh.edu>
2024-04-22 14:44:02 -0700

    add employee list to nav bar

--- projectmanagement/people/urls.py
+++ projectmanagement/people/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import*
 
 urlpatterns = [
-    path("home/", people_home),
+    path("home/", people_home,name='employee_list'),
     path('fetch_people_data/', fetch_people_data, name='fetch_people_data'),
 
 ]
